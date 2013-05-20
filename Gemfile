6c4d4c683447789e7bbe05a59c82ad227d72dd8f
Pedro Aranha <pedroaranha7@gmail.com>
2013-05-20 16:07:24 -0300

    adding meta-tags and some SEO

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.13'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'meta-tags', :require => 'meta_tags'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	set_meta_tags :title => 'Coding Nexus',
+                  :description => 'Various links, contact informations and portfolio showcasing my professional experience.',
+                  :keywords => 'Ruby on Rails, Ruby, Rails, Java, Javascript, jQuery, PostgreSQL, mySQL, Web Developing, Developer, Freelancer, Professional'
   end
 end
